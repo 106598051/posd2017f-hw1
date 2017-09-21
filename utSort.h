9266b7c6f8dd99dead55729c0431db0d8726cc31
Isida FKJ <jeffr500000@yahoo.com.tw>
2017-09-21 08:41:59 +0800

    Sort::sortByIncreasingPerimeter, Sort::sortByIncreasingArea, Sort::sortByDecreasingArea, Sort::sortByIncreasingCompactness completed homework1 finished

--- utSort.h
+++ utSort.h
@@ -6,20 +6,23 @@
 
 TEST (Sort, sortByIncreasingPerimeter) {
   Rectangle r(0, 0, 2, 3, "r");
-  ASSERT_NEAR(6.0, r.area(), 0.000001);
-  /*
   Circle c(0, 0, 1, "c");
   vertex vertex_A = {0, 0};
   vertex vertex_B = {0, 1};
   vertex vertex_C = {2, 0};
   Triangle t(vertex_A, vertex_B, vertex_C, "t");
-  ShapeMedia *shapes;
-  shapes.add(&r);
-  shapes.add(&c);
-  shapes.add(&t);*/
-
-
-
+  std::list<Shape *> shapes;
+  shapes.push_back(&r);
+  shapes.push_back(&c);
+  shapes.push_back(&t);
+  std::list<Shape *>::iterator i;
+  Sort::sortByIncreasingPerimeter(&shapes);
+  i = shapes.begin();
+  ASSERT_EQ(t.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(c.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(r.getShapeName(), (*i)->getShapeName());
 }
 
 TEST (Sort, sortByDecreasingPerimeter) {
@@ -35,33 +38,76 @@ TEST (Sort, sortByDecreasingPerimeter) {
   shapes.push_back(&t);
   std::list<Shape *>::iterator i;
   Sort::sortByDecreasingPerimeter(&shapes);
-  /*
-  for(i = shapes.begin(); i != shapes.end(); ++i) {
-    std::cout << (*i) << " ";
-  }
-  std::cout << std::endl;*/
-
   i = shapes.begin();
   ASSERT_EQ(r.getShapeName(), (*i)->getShapeName());
   i++;
   ASSERT_EQ(c.getShapeName(), (*i)->getShapeName());
   i++;
   ASSERT_EQ(t.getShapeName(), (*i)->getShapeName());
-
-
-
 }
 
 TEST (Sort, sortByIncreasingArea) {
-
+  Rectangle r(0, 0, 2, 3, "r");
+  Circle c(0, 0, 1, "c");
+  vertex vertex_A = {0, 0};
+  vertex vertex_B = {0, 1};
+  vertex vertex_C = {2, 0};
+  Triangle t(vertex_A, vertex_B, vertex_C, "t");
+  std::list<Shape *> shapes;
+  shapes.push_back(&r);
+  shapes.push_back(&c);
+  shapes.push_back(&t);
+  std::list<Shape *>::iterator i;
+  Sort::sortByIncreasingArea(&shapes);
+  i = shapes.begin();
+  ASSERT_EQ(t.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(c.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(r.getShapeName(), (*i)->getShapeName());
 }
 
 TEST (Sort, sortByDecreasingArea) {
-
+  Rectangle r(0, 0, 2, 3, "r");
+  Circle c(0, 0, 1, "c");
+  vertex vertex_A = {0, 0};
+  vertex vertex_B = {0, 1};
+  vertex vertex_C = {2, 0};
+  Triangle t(vertex_A, vertex_B, vertex_C, "t");
+  std::list<Shape *> shapes;
+  shapes.push_back(&r);
+  shapes.push_back(&c);
+  shapes.push_back(&t);
+  std::list<Shape *>::iterator i;
+  Sort::sortByDecreasingArea(&shapes);
+  i = shapes.begin();
+  ASSERT_EQ(r.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(c.getShapeName(), (*i)->getShapeName());
+  i++;
+  ASSERT_EQ(t.getShapeName(), (*i)->getShapeName());
 }
 
 TEST (Sort, sortByIncreasingCompactness) {
-
+    // Compactness = area/perimeter
+    Rectangle r(0, 0, 2, 3, "r");
+    Circle c(0, 0, 1, "c");
+    vertex vertex_A = {0, 0};
+    vertex vertex_B = {0, 1};
+    vertex vertex_C = {2, 0};
+    Triangle t(vertex_A, vertex_B, vertex_C, "t");
+    std::list<Shape *> shapes;
+    shapes.push_back(&r);
+    shapes.push_back(&c);
+    shapes.push_back(&t);
+    std::list<Shape *>::iterator i;
+    Sort::sortByIncreasingCompactness(&shapes);
+    i = shapes.begin();
+    ASSERT_EQ(t.getShapeName(), (*i)->getShapeName());
+    i++;
+    ASSERT_EQ(c.getShapeName(), (*i)->getShapeName());
+    i++;
+    ASSERT_EQ(r.getShapeName(), (*i)->getShapeName());
 }
 
 #endif
